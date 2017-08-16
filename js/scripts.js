86c7eba091c26a753c70fede6ba4fbdba20ecc0d
austin cummings and mike holland <mike@>
2017-08-16 10:54:43 -0700

    add jquery modify index and scripts, create logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
-  $("form#transportation_survey").submit(function(event){
+  $("#latin").submit(function(event){
     event.preventDefault();
-    
+    var sentence = ($("#userText").val().toUpperCase());
+    var sentence2 = sentence.split(" ");
+    var translatedSentence=[];
+
+    for (index = 0; index < sentence2.length; index +=1)   {
+      word=sentence2[index];
+      var brokenWord =word.split("");
+      letter=brokenWord.shift();
+
+
+        //insert if here to make sure its a letter//
+
+        //the if below identifies its not a vowel//
+       if (letter != 'A' && letter !='E' && letter !='I' && letter !='O' && letter !='U') {
+        letter = letter.concat("AY");
+        alert(letter);
+      }
+
+
+    }
+  });
+});
+
+
+//we will want join
